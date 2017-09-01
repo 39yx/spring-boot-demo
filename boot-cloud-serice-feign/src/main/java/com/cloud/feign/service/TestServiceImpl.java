8a260995d0493701d14b9a451f8d7d820a29739d
shana <984321681@qq.com>
2017-09-01 16:38:36 +0800

    熔断

--- boot-cloud-serice-feign/src/main/java/com/cloud/feign/service/TestServiceImpl.java
+++ boot-cloud-serice-feign/src/main/java/com/cloud/feign/service/TestServiceImpl.java
@@ -1,13 +1,14 @@
 package com.cloud.feign.service;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
+@RequestMapping("/test2")
 public class TestServiceImpl implements TestService {
 
 	@Override
-	public String getName(@RequestParam("name")String name) {
+	public String getName(String name) {
 		return "你得到是一个错误的名字 : " + name;
 	}
 
