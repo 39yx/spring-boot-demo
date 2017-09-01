aafa03de71794d859b78d874e1ee172855085d58
shana <984321681@qq.com>
2017-09-01 16:40:09 +0800

    熔断

--- boot-cloud-client/src/main/java/com/cloud/client/controller/TestController.java
+++ boot-cloud-client/src/main/java/com/cloud/client/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.cloud.client.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +15,16 @@ public class TestController {
 	private String port;
 	
 	@RequestMapping("/getName")
-	public String aa(@RequestParam("name") String name) {
+	public String aa(@RequestParam("name") String name,@RequestParam(value = "sleep",defaultValue = "1",required = false) Long sleep) {
 		if(name.equals("aa")) {
 			throw new RuntimeException("抛异常");
+		}
+		if(name.contains("kk")){
+			try {
+				TimeUnit.SECONDS.sleep(sleep);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		return port + " -->你要的名字哦: " + name;
 	}
